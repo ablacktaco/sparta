54554ff3eb801bf2a6af1ab4af1214a6a98ce50f
ABlackTaco <ablacktaco@gmail.com>
2019-11-25 09:58:53 +0800

    Initial Commit

---
+++ Sparta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sparta
+//
+//  Created by 陳姿穎 on 2019/11/25.
+//  Copyright © 2019 陳姿穎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
